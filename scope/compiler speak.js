516d10bff09014958fc7f66c8749115b49bf3a1f
Matthew Seidel <matthew.seidel@COL-MC-220523F.local>
2022-05-26 15:28:17 -0500

    chapter 1 and 2

---
+++ scope/compiler speak.js
@@ -0,0 +1,19 @@
+var students = [
+    { id: 1, name: "bruce", age: 40 },
+    { id: 2, name: "zoidberg", age: 22 },
+    { id: 3, name: "alex", age: 22 },
+    { id: 4, name: "daniel", age: 30 }
+];
+
+function getStudentName(studentID) {
+    for (let student of students) {
+        if (student.id === studentID) {
+            return student.name;
+        }
+    }
+}
+
+var nextStundet = getStudentName(2);
+
+console.log(nextStundet);
+//zoidberg
